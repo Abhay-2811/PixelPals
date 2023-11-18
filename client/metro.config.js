ddbb55f51d45426e8fe641fa7b8a35be16c84e91
CodeLuca <lucamarchal@gmail.com>
2023-11-18 12:48:12 +0300

    client

---
+++ client/metro.config.js
@@ -0,0 +1,35 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require("expo/metro-config");
+
+module.exports = (async () => {
+  const {
+    resolver: { assetExts },
+  } = await getDefaultConfig(__dirname);
+
+  return {
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+    resolver: {
+      assetExts: [
+        ...assetExts,
+        "obj",
+        "mtl",
+        "mp3",
+        "JPG",
+        "vrx",
+        "hdr",
+        "gltf",
+        "glb",
+        "bin",
+        "arobject",
+        "gif",
+      ],
+    },
+  };
+})();
